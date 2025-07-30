e81c0e73feb2ffed42d73f0e4e3fe82dd55ab616
Naman Kashyap <nkashyap@g2.com>
2025-07-30 19:06:23 +0530

    init

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import("fmt")
+// Also import file read/write
+
+func main() {
+	fmt.Println("Hello from mini_diff!\nNo LLMs were harmed in the making of this command line tool!")
+}
